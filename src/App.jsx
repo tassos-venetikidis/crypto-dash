8da5b55cd95921625f9a1baa8bb95171b218fbd7
Tassos Venetikidis <tassos.venetikidis@gmail.com>
2026-02-01 17:38:15 +0200

    Implemented a discrete route in the App component for the homepage via the set up of a pages folder and a Home component in it

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,6 @@
 import { useState, useEffect } from "react";
-import CoinCard from "./components/CoinCard.jsx";
-import LimitSelector from "./components/LimitSelector.jsx";
-import FilterInput from "./components/FilterInput.jsx";
-import SortSelector from "./components/SortSelector.jsx";
+import { Routes, Route } from "react-router";
+import Home from "./pages/Home.jsx";
 
 const API_URL = import.meta.env.VITE_API_URL;
 
@@ -32,50 +30,25 @@ function App() {
     fetchCoins();
   }, [limit]);
 
-  const filteredCoins = coins
-    .filter(
-      (coin) =>
-        coin.name.toLowerCase().includes(filter.toLowerCase()) ||
-        coin.symbol.toLowerCase().includes(filter.toLowerCase()),
-    )
-    .slice()
-    .sort((a, b) => {
-      switch (sortBy) {
-        case "market_cap_desc":
-          return b.market_cap - a.market_cap;
-        case "market_cap_asc":
-          return a.market_cap - b.market_cap;
-        case "price_desc":
-          return b.current_price - a.current_price;
-        case "price_asc":
-          return a.current_price - b.current_price;
-        case "change_desc":
-          return b.price_change_percentage_24h - a.price_change_percentage_24h;
-        case "change_asc":
-          return a.price_change_percentage_24h - b.price_change_percentage_24h;
-      }
-    });
-
   return (
-    <div>
-      <h1>🚀 Crypto Dash</h1>
-      {loading && <p>Loading...</p>}
-      {error && <div className="error">{error}</div>}
-      <div className="top-controls">
-        <FilterInput filter={filter} setFilter={setFilter} />
-        <LimitSelector limit={limit} setLimit={setLimit} />
-        <SortSelector sortBy={sortBy} setSortBy={setSortBy} />
-      </div>
-      {!loading && !error && (
-        <main className="grid">
-          {filteredCoins.length > 0 ? (
-            filteredCoins.map((coin) => <CoinCard key={coin.id} coin={coin} />)
-          ) : (
-            <p>No matching coins...</p>
-          )}
-        </main>
-      )}
-    </div>
+    <Routes>
+      <Route
+        path="/"
+        element={
+          <Home
+            coins={coins}
+            loading={loading}
+            error={error}
+            limit={limit}
+            filter={filter}
+            sortBy={sortBy}
+            setLimit={setLimit}
+            setFilter={setFilter}
+            setSortBy={setSortBy}
+          />
+        }
+      />
+    </Routes>
   );
 }
 
